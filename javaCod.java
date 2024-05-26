c16ee4c33a6f9e50e69254032338392bee97147a
Ismail <ismailtufekci14@gmail.com>
2024-05-26 18:45:13 +0200

    popraven red

--- javaCod.java
+++ javaCod.java
@@ -59,35 +59,35 @@ public class SILab2 {
             Item item = allItems.get(i);//5
             if (item.getName() == null || item.getName().length() == 0){ //6
                 item.setName("unknown"); //7
-            }//8
-            if (item.getBarcode() != null){//9
-                String allowed = "0123456789"; //10
-                char chars[] = item.getBarcode().toCharArray(); //11
-                for (int j = 0; j < item.getBarcode().length(); j++){ //12
-                    char c = item.getBarcode().charAt(j); //13
-                    if (allowed.indexOf(c) == -1){ //14
-                        throw new RuntimeException("Invalid character in item barcode!"); //15
+            }
+            if (item.getBarcode() != null){//8
+                String allowed = "0123456789"; //9
+                char chars[] = item.getBarcode().toCharArray(); //10
+                for (int j = 0; j < item.getBarcode().length(); j++){ //11
+                    char c = item.getBarcode().charAt(j); //12
+                    if (allowed.indexOf(c) == -1){ //13
+                        throw new RuntimeException("Invalid character in item barcode!"); //14
                     }
-                }// 16
-                if (item.getDiscount() > 0){ //17
-                    sum += item.getPrice()*item.getDiscount();//18
+                }
+                if (item.getDiscount() > 0){ // 15 
+                    sum += item.getPrice()*item.getDiscount();//16
                 }
                 else {
-                    sum += item.getPrice(); //19
+                    sum += item.getPrice(); //17
                 }
             }
             else {
-                throw new RuntimeException("No barcode!"); //20
+                throw new RuntimeException("No barcode!"); //18
             }
             if (item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcode().charAt(0) == '0'){ //21 
-                sum -= 30; //22
+                sum -= 30; //19
             }
         }
-        if (sum <= payment){ //23
-            return true; //24
+        if (sum <= payment){ //20
+            return true; //21
         }
         else {
-            return false; //25
+            return false; //22
         }
     }
-}//26
+}//23
